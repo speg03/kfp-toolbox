a123b7ed337a24269a3cbc5fa90efeb6088b4a4f
Takahiro Yano <speg03@gmail.com>
2022-04-09 17:15:18 +0900

    Version from importlib.metadata

--- kfp_toolbox/__init__.py
+++ kfp_toolbox/__init__.py
@@ -1,1 +1,5 @@
-__version__ = "0.0.0"
+from importlib.metadata import version
+
+from .decorators import spec  # noqa: F401
+
+__version__ = version("kfp-toolbox")
--- tests/test_kfp_toolbox.py
+++
@@ -1,5 +0,0 @@
-from kfp_toolbox import __version__
-
-
-def test_version():
-    assert __version__ == "0.0.0"
