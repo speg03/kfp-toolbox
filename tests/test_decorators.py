ff2bdcd05ecbf49342b38455a113b33f9b70008e
Takahiro Yano <speg03@gmail.com>
2022-04-10 18:59:20 +0900

    Add examples as tests

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -6,7 +6,7 @@ from kfp.v2 import compiler, dsl
 from kfp_toolbox import spec
 
 
-def test_spec(tmp_path):
+def test_spec_as_decorator(tmp_path):
     @spec(
         name="Echo Component",
         cpu="2",
@@ -40,3 +40,59 @@ def test_spec(tmp_path):
     assert container["resources"]["accelerator"]["type"] == "NVIDIA_TESLA_T4"
     assert task["taskInfo"]["name"] == "Echo Component"
     assert task["cachingOptions"]["enableCache"] is True
+
+
+def test_spec_as_function(tmp_path):
+    @dsl.component
+    def echo() -> str:
+        return "hello, world"
+
+    @dsl.pipeline(name="echo-pipeline")
+    def echo_pipeline():
+        spec(
+            name="Echo Component",
+            cpu="2",
+            memory="16G",
+            gpu="1",
+            accelerator="NVIDIA_TESLA_T4",
+            caching=True,
+        )(echo)()
+
+    pipeline_path = os.fspath(tmp_path / "pipeline.json")
+    compiler.Compiler().compile(pipeline_func=echo_pipeline, package_path=pipeline_path)
+
+    with open(pipeline_path, "r") as f:
+        pipeline_json = json.load(f)
+
+    container = pipeline_json["pipelineSpec"]["deploymentSpec"]["executors"][
+        "exec-echo"
+    ]["container"]
+    task = pipeline_json["pipelineSpec"]["root"]["dag"]["tasks"]["echo"]
+
+    assert container["resources"]["cpuLimit"] == 2.0
+    assert container["resources"]["memoryLimit"] == 16.0
+    assert container["resources"]["accelerator"]["count"] == "1"
+    assert container["resources"]["accelerator"]["type"] == "NVIDIA_TESLA_T4"
+    assert task["taskInfo"]["name"] == "Echo Component"
+    assert task["cachingOptions"]["enableCache"] is True
+
+
+def test_spec_caching(tmp_path):
+    @spec(caching=False)
+    @dsl.component
+    def echo() -> str:
+        return "hello, world"
+
+    @dsl.pipeline(name="echo-pipeline")
+    def echo_pipeline():
+        echo()
+
+    pipeline_path = os.fspath(tmp_path / "pipeline.json")
+    compiler.Compiler().compile(pipeline_func=echo_pipeline, package_path=pipeline_path)
+
+    with open(pipeline_path, "r") as f:
+        pipeline_json = json.load(f)
+
+    task = pipeline_json["pipelineSpec"]["root"]["dag"]["tasks"]["echo"]
+
+    assert "enableCache" not in task["cachingOptions"]
