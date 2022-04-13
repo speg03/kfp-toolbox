1214cc80b450d21c9a7b3a4907dfbc3c87d0b6d7
Takahiro Yano <speg03@gmail.com>
2022-04-13 16:28:44 +0900

    Add Python 3.7 support

--- kfp_toolbox/__init__.py
+++ kfp_toolbox/__init__.py
@@ -1,5 +1,8 @@
-from importlib.metadata import version
-
 from .decorators import spec  # noqa: F401
+
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version  # type: ignore
 
 __version__ = version("kfp-toolbox")
