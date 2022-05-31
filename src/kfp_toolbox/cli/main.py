41f8a44d774a0e0288112a98ca59b92a7f96a277
Takahiro Yano <speg03@gmail.com>
2022-05-31 13:01:42 +0900

    Change the entry point for CLI
    from a normal function to click.group

--- src/kfp_toolbox/cli/main.py
+++ src/kfp_toolbox/cli/main.py
@@ -6,19 +6,14 @@ from .. import versions
 from . import submit
 
 
-@click.group(invoke_without_command=True)
+@click.group(commands=[submit.submit], invoke_without_command=True)
 @click.help_option("-h", "--help")
 @click.option("-V", "--version", is_flag=True, help="Show the version and exit.")
 @click.pass_context
-def cli(ctx: click.Context, version: bool):
+def main(ctx: click.Context, version: bool):
     if version:
         print(versions.version_string())
         sys.exit(0)
     elif ctx.invoked_subcommand is None:
         print(ctx.get_help())
         sys.exit(0)
-
-
-def main():
-    cli.add_command(submit.submit)
-    cli()
--- tests/cli/test_main.py
+++ tests/cli/test_main.py
@@ -1,32 +1,19 @@
-import io
-from unittest.mock import patch
-
-import pytest
 from click.testing import CliRunner
 
 from kfp_toolbox.cli import main
 
 
-@patch("sys.argv", ["kfp-toolbox"])
-@patch("sys.stdout", new_callable=io.StringIO)
-def test_main(mock_stdout):
-    with pytest.raises(SystemExit):
-        main.main()
-
-    assert mock_stdout.getvalue().startswith("Usage: ")
-
-
-def test_cli():
+def test_main():
     runner = CliRunner()
-    result = runner.invoke(main.cli)
+    result = runner.invoke(main.main)
 
     assert result.exit_code == 0
     assert result.output.startswith("Usage: ")
 
 
-def test_cli_version():
+def test_main_version():
     runner = CliRunner()
-    result = runner.invoke(main.cli, ["--version"])
+    result = runner.invoke(main.main, ["--version"])
 
     assert result.exit_code == 0
     assert result.output.startswith("kfp-toolbox version ")
