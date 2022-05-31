bb597687e53b1075dc8d74005974344c807818d6
Takahiro Yano <speg03@gmail.com>
2022-05-31 13:02:44 +0900

    Reduce useless function imports

--- src/kfp_toolbox/versions.py
+++ src/kfp_toolbox/versions.py
@@ -1,11 +1,11 @@
 try:
-    from importlib.metadata import version
+    import importlib.metadata as metadata
 except ImportError:
-    from importlib_metadata import version  # type: ignore
+    import importlib_metadata as metadata  # type: ignore
 
-kfp_toolbox_version = version("kfp-toolbox")
-kfp_version = version("kfp")
-google_cloud_aiplatform_version = version("google-cloud-aiplatform")
+kfp_toolbox_version = metadata.version("kfp-toolbox")
+kfp_version = metadata.version("kfp")
+google_cloud_aiplatform_version = metadata.version("google-cloud-aiplatform")
 
 
 def version_string() -> str:
