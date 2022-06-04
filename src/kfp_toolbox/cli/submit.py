b23163edbc394a4fb35cd12ebf61d6401750c317
Takahiro Yano <speg03@gmail.com>
2022-06-04 14:46:03 +0900

    Implement submit with some arguments

--- src/kfp_toolbox/cli/submit.py
+++ src/kfp_toolbox/cli/submit.py
@@ -1,6 +1,9 @@
-import sys
+import argparse
+from typing import Sequence
 
 import click
+
+from kfp_toolbox import pipelines
 
 
 @click.command(add_help_option=False)
@@ -9,12 +12,54 @@ import click
     "-f",
     "--pipeline-file",
     type=click.Path(exists=True, dir_okay=False),
-    help="Path of the pipeline package file.",
+    help="Path of the pipeline package file. (required)",
 )
+@click.option("--endpoint", help="Endpoint of the KFP API service to connect.")
+@click.option("-e", "--experiment-name", help="Experiment name of the run.")
+@click.option("--pipeline-root", help="The root path of the pipeline outputs.")
+@click.argument("pipeline_parameters", nargs=-1)
 @click.pass_context
-def submit(ctx: click.Context, help: bool, pipeline_file: str):
+def submit(
+    ctx: click.Context,
+    help: bool,
+    pipeline_file: str,
+    endpoint: str,
+    experiment_name: str,
+    pipeline_root: str,
+    pipeline_parameters: Sequence[str],
+):
+    """Submit a pipeline job from the pipeline package file."""
+    parser = argparse.ArgumentParser(add_help=False, usage=argparse.SUPPRESS)
+    parameters_group = parser.add_argument_group("Pipeline parameters")
+    if pipeline_file:
+        pipeline = pipelines.load_pipeline_from_file(pipeline_file)
+        for parameter in pipeline.parameters:
+            sanitized_name = parameter.name.replace("_", "-").lstrip("-").rstrip("-")
+            required = parameter.default is None
+            parameters_group.add_argument(
+                f"--{sanitized_name}",
+                type=parameter.type,
+                default=parameter.default,
+                dest=parameter.name,
+                help="(required)" if required else "default: %(default)s",
+                required=required,
+            )
+
     if help:
         print(ctx.get_help())
-        sys.exit(0)
+        print()
+        parser.print_help()
+        ctx.exit(0)
+    elif not pipeline_file:
+        ctx.fail("The --pipeline-file option must be specified.")
 
-    click.echo(pipeline_file)
+    args = parser.parse_args(pipeline_parameters or [])
+    arguments_dict = vars(args)
+
+    pipelines.submit_pipeline_job(
+        pipeline_file=pipeline_file,
+        endpoint=endpoint,
+        experiment_name=experiment_name,
+        pipeline_root=pipeline_root,
+        arguments=arguments_dict,
+    )
--- tests/cli/test_submit.py
+++ tests/cli/test_submit.py
@@ -1,6 +1,55 @@
+import os
+from unittest.mock import patch
+
 from click.testing import CliRunner
+from kfp.v2 import compiler, dsl
 
 from kfp_toolbox.cli import submit
+
+
+@patch("kfp_toolbox.pipelines.submit_pipeline_job")
+def test_submit(mock_submit_pipeline_job, tmp_path):
+    @dsl.component
+    def echo() -> str:
+        return "hello, world"
+
+    @dsl.pipeline(name="echo-pipeline")
+    def echo_pipeline(param: int = 1, weird_param__: str = "Default String"):
+        echo()
+
+    pipeline_path = os.fspath(tmp_path / "pipeline.json")
+    compiler.Compiler().compile(pipeline_func=echo_pipeline, package_path=pipeline_path)
+
+    runner = CliRunner()
+    result = runner.invoke(
+        submit.submit,
+        [
+            "--pipeline-file",
+            pipeline_path,
+            "--",
+            "--param",
+            "5",
+            "--weird-param",
+            "Passed Value",
+        ],
+    )
+
+    assert result.exit_code == 0
+    mock_submit_pipeline_job.assert_called_once_with(
+        pipeline_file=pipeline_path,
+        endpoint=None,
+        experiment_name=None,
+        pipeline_root=None,
+        arguments={"param": 5, "weird_param__": "Passed Value"},
+    )
+
+
+def test_submit_with_no_pipelie_files():
+    runner = CliRunner()
+    result = runner.invoke(submit.submit)
+
+    assert result.exit_code != 0
+    assert "Error: The --pipeline-file option must be specified." in result.output
 
 
 def test_submit_help():
