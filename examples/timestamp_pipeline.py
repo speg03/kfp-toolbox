c1d9b3fddf9a5637a2e782e1d159a3dbfc2fa8a7
Takahiro Yano <speg03@gmail.com>
2022-09-10 23:14:25 +0900

    Add a example for the timestamp component

--- examples/timestamp_pipeline.py
+++ examples/timestamp_pipeline.py
@@ -6,3 +6,4 @@ from kfp_toolbox.components import timestamp
 @dsl.pipeline(name="timestamp-pipeline")
 def timestamp_pipeline():
     timestamp()
+    timestamp(format="%Y%m%d", prefix="examples", postfix="results", separator="/")
