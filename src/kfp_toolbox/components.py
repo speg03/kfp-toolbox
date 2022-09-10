5c00166f1ee00cf6da775b837aa974412a805d6d
Takahiro Yano <speg03@gmail.com>
2022-09-11 00:49:14 +0900

    Add a docstring for timestamp component

--- src/kfp_toolbox/components.py
+++ src/kfp_toolbox/components.py
@@ -23,3 +23,30 @@ def timestamp(
     if postfix:
         time_string = separator.join([time_string, postfix])
     return time_string
+
+
+timestamp.__doc__ = """Generate a time string in a specified format.
+
+    It adds a :attr:`prefix` or :attr:`postfix` to the time string generated from the
+    format, along with a :attr:`separator`. :attr:`tz_offset` can be specified to
+    represent a time zone other than UTC.
+
+    This function is used as a component. Since caching is disabled by default, it is
+    executed each time at the current time unless caching is enabled in the pipeline.
+
+    Args:
+        format (str, optional): Time format string. Strings that cannot be interpreted
+            as a format string are output as is. Defaults to "%Y%m%d%H%M%S".
+        prefix (Optional[str], optional): A prefix string to be prepended to the format
+            string. Defaults to None.
+        postfix (Optional[str], optional): A postfix string to be added after the
+            format string. Defaults to None.
+        separator (str, optional): Separator to be added between the format string,
+            prefix and postfix. Defaults to "-".
+        tz_offset (int, optional): Time zone offset specified in hours. Defaults to 0.
+
+    Returns:
+        str: The current time string represented by a format string. A prefix and
+        postfix string separated by a separator is appended.
+
+    """
