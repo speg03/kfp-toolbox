974bde40560be2410353993c93dedd2e28637497
Takahiro Yano <speg03@gmail.com>
2022-12-13 17:58:23 +0900

    Temporarily ignore all flake8 warnings for this file

--- src/kfp_toolbox/__init__.py
+++ src/kfp_toolbox/__init__.py
@@ -1,3 +1,7 @@
+# Ignore all flake8 warnings until the following issue is resolved
+# https://github.com/charliermarsh/ruff/issues/1052
+# flake8: noqa
+
 # "_version.py" is automatically generated when building a package.
 from ._version import __version__  # noqa: F401
 from .decorators import (  # noqa: F401
